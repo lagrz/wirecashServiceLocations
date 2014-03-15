8d64ca84ad6c411332cf3cbe5d6e26b0d50bb5c4
lagrz <roarbort@gmail.com>
2014-03-14 22:31:20 -0700

    initial commit

---
+++ test/wcServiceLocationsGmapSpec.js
@@ -0,0 +1,53 @@
+/*globals asyncTest,deepEqual,equal,expect,module,notDeepEqual,notEqual,notStrictEqual,ok,QUnit,raises,start,stop,strictEqual,test */
+var config;
+
+module('Test the google maps api loader', {
+    setup: function () {
+        stop();
+        $.get('../config/maps_api.json').done(function (data) {
+            config = data;
+            start();
+        });
+    }
+});
+
+test('Correctly builds global callback', function () {
+    expect(1);
+    var cb = jQuery.WCServiceListGmap.loader.buildGlobalCallback(config);
+    ok(window[cb], 'Global callback exist');
+});
+
+test('Correctly builds url for maps api', function () {
+    expect(3);
+
+    config = {
+        key: 'test',
+        sensor: '123'
+    };
+    var cb = jQuery.WCServiceListGmap.loader.buildGlobalCallback(config);
+    var url = jQuery.WCServiceListGmap.loader.buildUrl(cb, config);
+
+    notEqual(url.indexOf('https://maps.googleapis.com/maps/api/js?'), -1, 'Contains base url');
+    notEqual(url.indexOf('key=' + config.key), -1, 'Contains key');
+    notEqual(url.indexOf('sensor=' + config.sensor), -1, 'Contains key');
+});
+
+asyncTest('Loads google maps api dynamically with callback', function () {
+    'ust strict';
+    expect(2);
+
+    strictEqual(window.google, undefined, 'No maps api is currently loaded');
+
+    //load the config first
+    $.get('../config/maps_api.json').done(function (data) {
+
+        //load the gmap api
+        jQuery.WCServiceListGmap.loader.init($.extend({}, data, {
+            gmapsLoaded: function () {
+                ok(window.google, 'Google is now loaded');
+                start();
+            }
+        }));
+
+    });
+});
